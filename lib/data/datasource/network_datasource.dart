184d780185249735651c58ac79d14cbb4d66329e
Oleksii Butenko <basepbutenko@gmail.com>
2022-12-22 20:56:56 +0200

    GetAllJobs

--- lib/data/datasource/network_datasource.dart
+++ lib/data/datasource/network_datasource.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:dio/dio.dart';
 import 'package:workit/domain/model/company.dart';
+import 'package:workit/domain/model/job.dart';
 
 class NetworkDataSource {
   Future<List<Company>> getAllCompanies() async {
@@ -18,6 +19,29 @@ class NetworkDataSource {
         String industry = jsonItems[i]["industry"];
         int id = jsonItems[i]["id"];
         result.add(Company(id, name, description, industry));
+      }
+      return Future.value(result);
+    } catch (e) {
+      print(e);
+      return Future.value([]);
+    }
+  }
+
+  Future<List<Job>> getAllJobs() async {
+    try {
+      Response<String> response =
+          await Dio().get('http://3.75.134.87/flutter/v1/jobs');
+      print(response);
+      List<Job> result = [];
+      final data = await json.decode(response.data!);
+      List jsonItems = data["result"];
+      for (var i = 0; i < jsonItems.length; i++) {
+        String title = jsonItems[i]["title"];
+        String description = jsonItems[i]["description"];
+        String city = jsonItems[i]["city"];
+        int id = jsonItems[i]["id"];
+        int companyId = jsonItems[i]["companyId"];
+        result.add(Job(id, companyId, title, description, city));
       }
       return Future.value(result);
     } catch (e) {
--- lib/data/job_repo_impl.dart
+++ lib/data/job_repo_impl.dart
@@ -1,11 +1,14 @@
+import 'package:workit/data/datasource/network_datasource.dart';
 import 'package:workit/domain/model/job.dart';
 import 'package:workit/domain/repo/job_repo.dart';
 
-class JobRepositoryImpl implements JobRepository{
+class JobRepositoryImpl implements JobRepository {
+  NetworkDataSource networkDataSource;
+
+  JobRepositoryImpl(this.networkDataSource);
+
   @override
   Future<List<Job>> getAllJobs() {
-    // TODO: implement getAllJobs
-    throw UnimplementedError();
+    return networkDataSource.getAllJobs();
   }
-
 }
