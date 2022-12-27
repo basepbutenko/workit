ecfd26a309324dcefb0a021ce72bca177ce63c7a
Oleksii Butenko <basepbutenko@gmail.com>
2022-12-27 21:23:24 +0200

    Companies Tab

--- lib/ui/companies/companies_tab.dart
+++ lib/ui/companies/companies_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'companies_bloc.dart';
@@ -19,13 +20,28 @@ class _CompaniesTabState extends State<CompaniesTab> {
       bloc: companiesBloc,
       builder: (context, state) {
         if (state is CompaniesLoadingState) {
-          return Container(
-            child: Text('loading'),
+          return Center(
+            child: CircularProgressIndicator(),
           );
         } else if (state is CompaniesLoadedState) {
-          return Container(
-            child: Text('loaded'),
-          );
+          return ListView.separated(
+              separatorBuilder: (context, index) {
+                return Divider();
+              },
+              itemCount: state.companies.length,
+              itemBuilder: (BuildContext ctxt, int index) {
+                return Row(
+                  children: [
+                    Column(
+                      children: [
+                        Text(state.companies[index].name),
+                        Text(state.companies[index].description),
+                        Text(state.companies[index].industry),
+                      ],
+                    ),
+                  ],
+                );
+              });
         }
         return Container(
           child: Text('companies'),
